052663c3d653c22b1ae0eb2570feaff4860459ab
Luiz <guilhermeoluiz04@gmail.com>
2023-02-11 10:33:26 -0300

    ..

--- Java/Hello_Word.java
+++ Java/Hello_Word.java
@@ -2,7 +2,7 @@ public class Hello_Word{
 
     public static void main(String[] args) {
         
-        System.out.println("Hello word, Sejam bem vindos!");
+        System.out.println("Hello word! \nSejam bem vindos!");
 
     }
 }
