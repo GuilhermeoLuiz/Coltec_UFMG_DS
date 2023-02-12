ab818b4dc322ebb903b5044f2be33bf7e7647a31
Luiz <guilhermeoluiz04@gmail.com>
2023-02-12 16:29:13 -0300

    Adiciona pasta de linguagem C, e insere Hello Word

---
+++ C/Hello_Word.c
@@ -0,0 +1,11 @@
+//Inclui bibliotecas e define constantes
+#include <stdio.h>
+#define SUCESSO 0
+
+int main(int argc, char** argv) {
+
+    //Imprimi Hello Word e realiza a funcao do programa
+    printf("Hello Word!");
+
+    return SUCESSO;
+}
