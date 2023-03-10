119acc1d9f99fecf4dfc592bc30c6e76c60420db
Guilherme Luiz T. Gonzaga <guilhermeoluiz04@gmail.com>
2023-03-10 11:17:52 -0300

    adicionando conferes

--- Java/AEDS_MenuRestaurante/Main.java
+++ Java/AEDS_MenuRestaurante/Main.java
@@ -2,13 +2,11 @@ package AEDS_MenuRestaurante;
 
 public class Main {
 //Trabalho feito por Guilherme Luiz e Leonardo Prado
-
-public class TestaRestaurante {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         
         //cria restaurante
         Restaurante r = new Restaurante("Buchin Cheio", "Brumadinho");
         r.menu();
     }
-}
+
 }
