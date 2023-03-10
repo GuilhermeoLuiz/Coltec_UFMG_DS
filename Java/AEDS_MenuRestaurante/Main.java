f5aca5cc693c128c8eec28d5fe802ce121e5098b
Guilherme Luiz T. Gonzaga <guilhermeoluiz04@gmail.com>
2023-03-10 07:53:24 -0300

    Upando arquivos pro git

--- Java/AEDS_MenuRestaurante/Main.java
+++ Java/AEDS_MenuRestaurante/Main.java
@@ -4,8 +4,8 @@ public class Main {
 //Trabalho feito por Guilherme Luiz e Leonardo Prado
 
 public class TestaRestaurante {
-    public static void main(String[] args) {
-
+    public void main(String[] args) {
+        
         //cria restaurante
         Restaurante r = new Restaurante("Buchin Cheio", "Brumadinho");
         r.menu();
