08c6ef449c3992d29db017f9bb8144644c267c6a
Guilherme Luiz T. Gonzaga <104567893+GuilhermeoLuiz@users.noreply.github.com>
2023-04-19 14:37:36 -0300

    Minimo e Maximo

---
+++ JS/MinMax.js
@@ -0,0 +1,25 @@
+function min(x, y){
+
+    if(x < y){
+        return x;
+    }
+    if(x > y){
+        return y;
+    } 
+    else{
+        return null;
+    }
+}
+
+function max(x, y){
+
+    if(x < y){
+        return y;
+    }
+    if(x > y){
+        return x;
+    } 
+    else{
+        return null;
+    }
+}
