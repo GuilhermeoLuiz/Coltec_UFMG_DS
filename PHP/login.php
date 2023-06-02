1f26dc6a1c16614fde2fed0465e639b0029e3d57
joaododaw <2020951945@teiacoltec.org>
2023-06-02 09:14:43 -0300

    Pasta php & formulario

---
+++ PHP/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+
+<head></head>
+
+<body>
+    <form action="index.php" method="POST">
+        Login: <input type="text" name="login"> <br>
+        Senha: <input type="password" name="senha"> <br>
+        <input type="submit" value="Log In">
+    </form>
+
+
+</body>
+
+
+</html>
